135c803dfceaf6e3dd1f9ebc58bcfdfb0a4a5062
NhatDang1102 <nezukosama247@gmail.com>
2025-10-18 18:07:15 +0700

    fix: Đổi lại theo db first

--- Main/Program.cs
+++ Main/Program.cs
@@ -1,25 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿using System;
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+{
+    Args = args,
+    ContentRootPath = AppContext.BaseDirectory
+});
+
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddJsonFile("appsettings.Secret.json", optional: true, reloadOnChange: true);
+
+var cs = builder.Configuration.GetConnectionString("Default");
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
